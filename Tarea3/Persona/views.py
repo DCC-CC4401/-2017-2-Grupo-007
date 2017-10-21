53fbe9a6cc4c34f93ce2120a5caf3640e492bb8a
PMiranda <pablo.miranda.a@gmail.com>
2017-10-21 19:42:05 -0400

    login listo

--- Tarea3/Persona/views.py
+++ Tarea3/Persona/views.py
@@ -1,10 +1,57 @@
-from django.contrib.auth import login, authenticate
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
+from Persona.models import Persona
+from Municipalidad.models import Municipalidad
+from ONG.models import ONG
 
 
 def home(request):
-    return render(request, 'Landing.html', {})
+    if request.POST:
+        print(request.POST)
+
+        username = request.POST.get('username')
+        password = request.POST.get('pass')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            print("Logged in")
+            print(user)
+            print(user.groups.all())
+
+            group = user.groups.all()[0].name
+
+            print(group)
+
+            if group == "Administrador" or group == "Persona":
+
+                persona = Persona.objects.get(usuario_id=user.id)
+
+                return render(request, 'Landing.html', {'persona': persona})
+
+            if group == "Municipalidad":
+
+                mun = Municipalidad.objects.get(usuario_id=user.id)
+
+                return HttpResponseRedirect(reverse('municipalidad:muni', args={'muni': mun}))
+
+            if group == "ONG":
+
+                ong = ONG.objects.get(usuario_id=user.id)
+
+                return HttpResponseRedirect('/')  ## TODO: implementar ONG
+
+        else:
+            return render(request, 'Landing.html', {'error': "Nombre de usuario o cotraseña invalidos"})
+
+    else:
+        logout(request)
+        return render(request, 'Landing.html', {})
 
 
 def signup(request):
--- Tarea3/Tarea3/urls.py
+++ Tarea3/Tarea3/urls.py
@@ -19,8 +19,8 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('Persona.urls', namespace='persona')),
-    url(r'^denuncia/', include('Denuncia.urls')),
-    url(r'^animal/', include('Animal.urls')),
-    url(r'^municipalidad/', include('Municipalidad.urls')),
+    url(r'^denuncia/', include('Denuncia.urls', namespace='denuncia')),
+    url(r'^animal/', include('Animal.urls', namespace='animal')),
+    url(r'^municipalidad/', include('Municipalidad.urls', namespace='municipalidad')),
 
 ]
